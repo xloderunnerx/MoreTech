ace9c525031dec458f320aeb91bf6b7928b0bf7e
Glav Mraz <xloderunnerx@gmail.com>
2021-10-10 06:13:30 +0300

    Instruction scree.

---
+++ Assets/Scripts/OpenUrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public string url;
+    public void Open() {
+        Application.OpenURL(url);
+    }
+}
